4c29ff7e7b58400cde5047e5f7b2a04e28f25b91
Alec Hannaford <alec322@gmail.com>
2023-07-08 21:30:08 -0700

    run model

---
+++ src/main/java/com/example/demo/model/Run.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+public class Run {
+}
