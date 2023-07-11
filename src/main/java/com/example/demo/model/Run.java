2370dc1e7a04a0c66ccd7d1220c161b930e4ff4d
Alec Hannaford <alec322@gmail.com>
2023-07-10 22:26:42 -0700

    Run model

--- src/main/java/com/example/demo/model/Run.java
+++ src/main/java/com/example/demo/model/Run.java
@@ -1,9 +1,50 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-
+@Entity
+@Table(name = "runs")
 public class Run {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "run_generator")
+    private long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="to,e")
+    private double time;
+
+    @ManyToOne(fetch=FetchType.LAZY, optional = false)
+    @JoinColumn(name = "run_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Route route;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
 }
