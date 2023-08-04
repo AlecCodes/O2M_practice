40955763e8cae1401de0feab69eb48b3e62b18f1
Alec Hannaford <alec322@gmail.com>
2023-08-03 18:36:43 -0700

    find run by id

--- src/main/java/com/example/demo/controller/RunController.java
+++ src/main/java/com/example/demo/controller/RunController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 import java.util.List;
+import java.util.Optional;
+
 import com.example.demo.model.Run;
 import com.example.demo.repository.RunRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,14 @@ public class RunController {
 
     @GetMapping("/runs")
     public List<Run> getRuns() {return runRepository.findAll();}
+
+    @GetMapping("/runs/{id}")
+    public Optional<Run> getRunsById(@PathVariable(value="id") Long id ){
+        Run run = runRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("COULD NOT FIND RUN W ID: " + id));
+        return runRepository.findById(id);
+    }
+
 
 
 
